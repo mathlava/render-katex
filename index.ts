cbcf6f3fffba8db8938a4756b7d794ac4654f6e2
Amadeus_vn <git@amas.dev>
2021-01-22 03:26:10 +0900

    fix css

--- index.ts
+++ index.ts
@@ -1,16 +1,14 @@
 import {renderToString} from 'katex'
 import express from 'express'
 const app = express()
+
 function source(tex: string) {
 	return `
 <html>
 <head><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.12.0/katex.min.css" /></head>
 <style>
 body {
-min-width: 5em;
-min-height: 3em;
-padding: 2em;
-font-size: 50px;
+font-size: 100px;
 display: flex;
 justify-content: center;
 align-items: center;
@@ -23,6 +21,7 @@ align-items: center;
 </html>
 `
 }
+
 app.get('/tex/:source', (req, res) => {
 	try {
 		res.send(source(req.params.source))
